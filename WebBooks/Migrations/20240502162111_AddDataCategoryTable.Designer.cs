7350be2408262ce25c86ff53587fbe8577ce6d4f
Robert <c.robertstancu@yahoo.com>
2024-05-02 19:50:26 +0300

    Added category table seed

--- WebBooks/Migrations/20240502162111_AddDataCategoryTable.Designer.cs
+++ WebBooks/Migrations/20240502162111_AddDataCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebBooks.Data;
 
@@ -10,9 +11,11 @@ using WebBooks.Data;
 namespace WebBooks.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240502162111_AddDataCategoryTable")]
+    partial class AddDataCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
