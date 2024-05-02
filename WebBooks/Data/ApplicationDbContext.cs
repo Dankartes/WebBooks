73623d1e7b0671e64d49745fc77d3ae39bd16196
Robert <c.robertstancu@yahoo.com>
2024-05-02 18:40:09 +0300

    Added db context and configuration

---
+++ WebBooks/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebBooks.Models;
+
+namespace WebBooks.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
