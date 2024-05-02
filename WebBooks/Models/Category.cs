dfafd589e611dbb4274c595652881cea79425ca4
Robert <c.robertstancu@yahoo.com>
2024-05-02 18:39:30 +0300

    Added category model

---
+++ WebBooks/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBooks.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
