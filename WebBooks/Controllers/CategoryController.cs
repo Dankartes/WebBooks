630965fbe0fa5b0f926ec8d5528ebcb2abe270e9
Robert <c.robertstancu@yahoo.com>
2024-05-03 16:44:11 +0300

    Added CRUD operations to categories

--- WebBooks/Controllers/CategoryController.cs
+++ WebBooks/Controllers/CategoryController.cs
@@ -26,19 +26,91 @@ namespace WebBooks.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            //if(obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name");
-            //}
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name");
+            }
 
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
-            return RedirectToAction("Index", "Category");
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index", "Category");
             }
 
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+            //Category? category1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category? category2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
+
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+            //Category? category1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category? category2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = _db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
