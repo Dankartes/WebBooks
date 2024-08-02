e03ffff9f25b616c38c57895ea30224d141a011a
Robert Stancu <c.robertstancu@yahoo.com>
2024-08-02 18:33:38 +0300

    Added ProductVM file to version control

---
+++ WebBooks.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+#nullable disable
+
+namespace WebBooks.Models.ViewModels;
+
+public class ProductVm
+{
+    public Product Product { get; set; }
+    [ValidateNever] public IEnumerable<SelectListItem> CategoryList { get; set; }
+}
